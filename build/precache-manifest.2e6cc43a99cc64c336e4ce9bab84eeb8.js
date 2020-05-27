b00cb7f01352cde587ad72b01ef2570298757547
abhishekvash <abhishekvash34@gmail.com>
2020-05-27 20:59:30 +0530

    Hosting  on gh-pages

---
+++ build/precache-manifest.2e6cc43a99cc64c336e4ce9bab84eeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd19d97c86b843d14aa8df4d1d25b5c",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "486f3f2c80fe52b6fd5a",
+    "url": "/covid-tracker/static/css/main.56e5a1fb.chunk.css"
+  },
+  {
+    "revision": "2b691180bb845f8dad06",
+    "url": "/covid-tracker/static/js/2.9dad0b81.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.9dad0b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486f3f2c80fe52b6fd5a",
+    "url": "/covid-tracker/static/js/main.fc53e539.chunk.js"
+  },
+  {
+    "revision": "4ab6cc6a6673a41c1175",
+    "url": "/covid-tracker/static/js/runtime-main.51b14c82.js"
+  }
+]);
